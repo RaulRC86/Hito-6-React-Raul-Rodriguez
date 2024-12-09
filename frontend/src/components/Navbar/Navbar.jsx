662222e1830c67dc3a9128a9b0f0f1faf8e7af15
Raúl <raul.rodriguez.c.2012@gmail.com>
2024-12-09 18:59:22 -0300

    eliminación de mayúsculas en rutas del Navbar

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -11,11 +11,11 @@ export const Navbar=() =>{
       <h4>Pizzería Mamma Mía!</h4>
       <Link to="/"><button className='boton1'>🍕Home</button></Link>
       <Link to="/profile"><button className='boton1'>🔓Profile</button></Link>
-      <Link to="/Login"><button className='boton1'>🔐Login</button></Link>
+      <Link to="/login"><button className='boton1'>🔐Login</button></Link>
       <button>🔒Logout</button>
-      <Link to="/Register"><button className='boton1'>🔐Register</button></Link>
+      <Link to="/register"><button className='boton1'>🔐Register</button></Link>
       <div className="totalCompra">
-      <Link to="/Cart"><button className='boton2'>🛒Total:$ {total.toFixed(0)}</button></Link>
+      <Link to="/cart"><button className='boton2'>🛒Total:$ {total.toFixed(0)}</button></Link>
     </div>
     </nav>
         </>
